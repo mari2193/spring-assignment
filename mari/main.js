bd870472b71b39e5f8f6bb924a0947ede84dbd04
Mari2102 <59822207+Mari2102@users.noreply.github.com>
2020-02-24 20:45:41 +0200

    Page with image slider
    Added PDF -button on top, automated image slider to the top of the  page

--- mari/main.js
+++ mari/main.js
@@ -1,20 +1,10 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(document).ready(function() {
+  $(".my-carousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
